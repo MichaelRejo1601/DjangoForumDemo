700c9932733c26680180e3b29909a2946fdf2ae9
Michael Rejo <ninjapenguin1601@gmail.com>
2020-01-14 09:49:23 -0800

    Added Models

--- Forum/models.py
+++ Forum/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    """A Post a User Makes"""
+    date = models.DateField()
+    time = models.TimeField()
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    author = models.CharField(max_length=100)
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.title
